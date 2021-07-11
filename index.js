c5696f27aed46c0b6173ede6f1246a13b21da726
R-Harishkumar <63601427+R-Harishkumar@users.noreply.github.com>
2021-07-11 20:37:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const jokeText = document.getElementById("joke");
+const jokeBtn = document.getElementById("btn");
+
+jokeBtn.addEventListener("click", generateJokes)
+
+generateJokes()
+
+async function generateJokes() {
+    const res = await fetch('https://v2.jokeapi.dev/joke/Programming')
+
+    const data = await res.json();
+    // console.log(data)
+    // console.log(data.joke)
+    // console.log(data.delivery)
+    let joke = " "
+
+    if (data.joke === undefined) {
+        joke = `${data.setup} <br/> ${data.delivery}`
+    } else {
+        joke = data.joke
+    }
+
+    jokeText.innerHTML = joke;
+
+
+}
